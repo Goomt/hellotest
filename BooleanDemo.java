a43ce9332407d9f6c98ade3d0e22639aec9cb847
Goomt <serosm93@gmail.com>
2022-05-03 10:45:23 +0900

    test test

---
+++ BooleanDemo.java
@@ -0,0 +1,25 @@
+package org.opentutorials.javatutorials.ecilpse;
+import java.util.Scanner;
+
+public class BooleanDemo {
+	public static void main(String[] args) {
+		if(true&&true)
+			System.out.println(1);
+		if(true&&false)
+			System.out.println(2);
+		if(false&&true)
+			System.out.println(3);
+		if(false&&true)
+			System.out.println(4);
+	
+//		Scanner sc = new Scanner(System.in);
+//		String ids = 
+//		String id = args[0];
+//		String password = args[1];
+//		if(id.equals("egoing")&& password.equals("1111"))
+//			System.out.println("right");
+//		else
+//			System.out.println("wrong");
+//	
+}
+}
