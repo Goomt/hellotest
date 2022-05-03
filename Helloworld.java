36c515f2265b7940b10dd31c64f65dbcf99b2464
Goomt <serosm93@gmail.com>
2022-05-03 11:21:41 +0900

    change

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.ecilpse;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello world");
+	}
+
+}
